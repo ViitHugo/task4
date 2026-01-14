5edb5dc7233cd9abde90fdb122e24352db461835
Vitor Hugo <111470753+ViitHugo@users.noreply.github.com>
2026-01-14 11:49:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include("header.php");
+include("nav.php");
+?>
+
+
+
+<title>Início - Eneba</title>
+
+
+
+
+
+
+<?php include("../task4/footer.php") ?>
---
+++ nav.php
@@ -0,0 +1,48 @@
+    <!-------------------------------------------------------------------------------------------------------------------------------->
+    <!-------------------------------------------------------------------------------------------------------------------------------->
+    <!-------------------------------------------------------------------------------------------------------------------------------->
+    <div id="div_nav_primary" class="text-light">
+    <nav id="nav_primary" class="container d-flex justify-content-between align-items-center fs-5 py-2"> <!-------------------- NAVEGAÇÃO PRIMÁRIA -->
+        
+        <div>
+        <img src="img/main_icon_eneba.png" class="img-fluid" alt="main icon eneba" width="75px" height="75px">
+        <span>Eneba</span>
+        </div>
+
+        <div id="div_search_bar" class="d-flex justify-content-between"> <!-------------------------------------------- BARRA DE PESQUISA -->
+
+        <form action="#" method="GET">
+            <input class="bg-transparent" placeholder="Pesquise por jogos, recargas, gift-cards, licenças e mais">
+            <button class="bi bi-search"></button>
+        </form>
+
+        </div>
+
+        <span><i class="bi bi-globe"></i> Idioma | XX</span>
+        <span><i class="bi bi-heart"></i></span>
+        <span><i class="bi bi-cart"></i></span>
+        <span><i class="bi bi-person"></i> Entrar / Registrar</span>
+
+    </nav>
+    </div>
+
+    <!-------------------------------------------------------------------------------------------------------------------------------->
+    <!-------------------------------------------------------------------------------------------------------------------------------->
+    <!-------------------------------------------------------------------------------------------------------------------------------->
+
+    <div id="div_nav_secundary" class="text-light">
+    <nav id="nav_secundary" class="container d-flex justify-content-between align-items-center"> <!---------------------------------------------- NAVEGAÇÃO SECUNDÁRIA -->
+
+        <span><i class="bi bi-list"></i> Categorias</span>
+
+        <span>Cheap Games</span>
+        <span>Windows Keys</span>
+        <span>Random Keys</span>
+        <span>Top Deals</span>
+        <span>GOTY '25</span>
+        <span>Valorant</span>
+        <span>Roblox</span>
+        <span>PUBG UC</span>
+
+    </nav>
+    </div>
