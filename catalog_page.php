9827cd456f48304bac4dee704d1b7d84fabff91a
Vitor Hugo <111470753+ViitHugo@users.noreply.github.com>
2026-01-26 11:46:25 -0300

    Add files via upload

--- catalog_page.php
+++ catalog_page.php
@@ -11,6 +11,12 @@
     <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="2" aria-label="Slide 3"></button>
     <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="6" aria-label="Slide 7"></button>
+    <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="7" aria-label="Slide 8"></button>
+    <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="8" aria-label="Slide 9"></button>
+    <button type="button" data-bs-target="#carouselTopSponsers" data-bs-slide-to="9" aria-label="Slide 10"></button>
   </div>
   
   <div class="carousel-inner">
@@ -19,26 +25,62 @@
     </div>
 
     <div id="container-xbox-card" class="carousel-item">
-      <img id="img-xbox-card" src="../task4/img/cards/card_xbox.png" class="img-carousel-sponsers d-block" alt="xbox card">
+      <img id="img-xbox-card" src="../task4/img/cards/card_xbox2.png" class="img-carousel-sponsers d-block" alt="xbox card">
+    </div>
+
+    <div id="container-nintendo-switch-card" class="carousel-item">
+      <img id="img-nintendo-switch-card" src="../task4/img/cards/card_nintendo_switch.png" class="img-carousel-sponsers d-block" alt="nintendo switch card">
+    </div>
+
+    <div id="container-playstation-card" class="carousel-item">
+      <img id="img-playstation-card" src="../task4/img/cards/card_playstation.png" class="img-carousel-sponsers d-block" alt="playstation card">
+    </div>
+
+    <div id="container-google-play-card" class="carousel-item">
+      <img id="img-google-play-card" src="../task4/img/cards/card_google_play.png" class="img-carousel-sponsers d-block" alt="google play card">
+    </div>
+
+    <div id="container-itunes-card" class="carousel-item">
+      <img id="img-itunes-card" src="../task4/img/cards/card_itunes.png" class="img-carousel-sponsers d-block" alt="itunes card">
+    </div>
+
+    <div id="container-microsoft-card" class="carousel-item">
+      <img id="img-microsoft-card" src="../task4/img/cards/card_microsoft.png" class="img-carousel-sponsers d-block" alt="microsoft card">
     </div>
 
     <div id="container-mojang-card" class="carousel-item">
       <img id="img-mojang-card" src="../task4/img/cards/card_mojang.png" class="img-carousel-sponsers d-block" alt="mojang card">
     </div>
 
-    <div id="container-microsoft-card" class="carousel-item">
-      <img id="img-microsoft-card" src="../task4/img/cards/card_microsoft.png" class="img-carousel-sponsers d-block" alt="microsoft card">
+    <div id="container-pichau-card" class="carousel-item">
+      <img id="img-pichau-card" src="../task4/img/cards/card_pichau.png" class="img-carousel-sponsers d-block" alt="pichau card">
+    </div>
+
+    <div id="container-dreamworks-card" class="carousel-item">
+      <img id="img-dreamworks-card" src="../task4/img/cards/card_dreamworks.png" class="img-carousel-sponsers d-block" alt="dreamworks card">
     </div>
   </div>
 
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselTopSponsers" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="carousel-control bi bi-caret-left-fill" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselTopSponsers" data-bs-slide="next">
+    <span class="carousel-control bi bi-caret-right-fill" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+
+  <!--
+                      ///////////////ALTERNATIVA//////////////////
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselTopSponsers" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon text-danger" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselTopSponsers" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
+  -->
 </div>
 
 <!------------------------------------------------------------------------------------------------------------------------------------->
@@ -46,73 +88,123 @@
 <!------------------------------------------------------------------------------------------------------------------------------------->
 
 <div id="div_subnav_catalog"> <!--class="bg-primary"-->
-<nav class="container d-flex flex-row gap-5 align-items-center overflow-x-auto py-3">
-    <button class="btn bi bi-caret-left fs-5 text-light"></button>
-
-    <a class="btn d-flex flex-column fs-5 text-light">
+<div class="container d-flex flex-row gap-2">
+  <button class="btn-scroll-subnav-catalog btn bi bi-caret-left"></button>
+  
+  <nav id="subnav_catalog" class="d-flex flex-row gap-4 align-items-center py-3 text-nowrap">
+      <a class="btn-subnav-catalog btn d-flex flex-column">
         <i class="bi bi-controller"></i>
         <span>Jogos</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
-        <i class="bi bi-controller"></i>
-        <span>Jogos</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
-        <i class="bi bi-controller"></i>
-        <span>Jogos</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
-        <i class="bi bi-controller"></i>
-        <span>Jogos</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-box2-heart"></i>
+        <span>Gift Cards</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
         <i class="bi bi-gift"></i>
         <span>Game Cards</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
-        <i class="bi bi-gift"></i>
-        <span>Gift Cards</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
         <i class="bi bi-key"></i>
         <span>Chaves</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
         <i class="bi bi-steam"></i>
         <span>Steam</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
         <i class="bi bi-xbox"></i>
         <span>Xbox</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
-        <i class="bi bi-microsoft"></i>
-        <span>Microsoft</span>
-    </a>
-    <a class="btn d-flex flex-column fs-5 text-light">
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-nintendo-switch"></i>
+        <span>Nintendo</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
         <i class="bi bi-playstation"></i>
         <span>Playstation</span>
-    </a>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-google-play"></i>
+        <span>Google Play</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-apple"></i>
+        <span>Apple</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-microsoft"></i>
+        <span>Microsoft</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-x-square"></i>
+        <span>Coming soon</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-x-square"></i>
+        <span>Coming soon</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-x-square"></i>
+        <span>Coming soon</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-x-square"></i>
+        <span>Coming soon</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-x-square"></i>
+        <span>Coming soon</span>
+      </a>
+      <a class="btn-subnav-catalog btn d-flex flex-column">
+        <i class="bi bi-x-square"></i>
+        <span>Coming soon</span>
+      </a>
+  </nav>
 
+  <button class="btn-scroll-subnav-catalog btn bi bi-caret-right"></button>
+</div>
+</div>
 
+<!------------------------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------------------------------->
 
+<div class="container text-light">
+<h2 class="py-3 mb-3">Recomendações para você</h1>
+<div class="d-flex justify-content-between">
+  <div class="recommended-card d-flex justify-content-center flex-end col">
+    <span class="bg-primary mt-5">Steam</span>
 
+  
+  </div>
+  <div class="recommended-card d-flex justify-content-center flex-end col">
+    <span class="bg-primary mt-5">Steam</span>
 
+  
+  </div>
+  <div class="recommended-card d-flex justify-content-center flex-end col">
+    <span class="bg-primary mt-5">Steam</span>
 
-
-
-    <button class="btn bi bi-caret-right fs-5 text-light"></button>
-
-
-
-
-
-
-</nav>
+  
+  </div>
 </div>
 
 
 
+
+<h1 class="mt-5">Mais Vendidos</h1>
+
+
+<h1>Gift Cards de Apps Favoritos</h1>
+
+
+<h1>Ranking dos Melhores</h1>
+
+
+
+<h1>Categorias</h1>
+</div>
 
 
 
